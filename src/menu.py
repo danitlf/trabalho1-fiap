04292ae770b0995ee58a9ca4a13ff4831a9f15af
Daniel Tavares de Lima Freitas <danilimah18@gmail.com>
2025-03-07 20:49:57 -0300

    create storage and refactor

---
+++ src/menu.py
@@ -0,0 +1,10 @@
+
+
+def exibir_menu():
+    print("\nMenu de Opções:")
+    print("1. Adicionar dados de plantio")
+    print("2. Exibir dados de plantio")
+    print("3. Atualizar dados de plantio")
+    print("4. Deletar dados de plantio")
+    print("5. Sair")
+
